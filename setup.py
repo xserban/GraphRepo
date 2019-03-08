a43ec42ea8e4fe3a6b23e1a8ea63848c3b2770d0
NullConvergence <a.serban@cs.ru.nl>
2019-03-08 23:49:59 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def read_reqs(filename: str):
+  req = []
+  with open(filename, 'r') as f:
+    for line in f:
+      if line.strip() and not line.startswith('-r'):
+        req.append(line.strip())
+
+  return req
+
+
+install_requires = read_reqs("requirements.txt")
 
 setup(name="graphrepo",
-      version="0.1",
+      version="0.2",
       description="A tool that maps a Github repo to Neo4j",
       url="https://github.com/NullConvergence/GraphRepo",
-      author="NullConvergence",
-      license='Apache2',
-      install_requires=[
-          'py2neo',
-          'pydriller',
-      ],
-      packages=find_packages())
+      license='Apache License',
+      python_requires='>=3.4',
+      install_requires=install_requires,
+      packages=find_packages(),
+      package_dir={'graphrepo': 'graphrepo'})
