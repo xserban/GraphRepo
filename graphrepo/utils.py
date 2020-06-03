4b1aea61ab25aca4a98e1321e95d79d8d6fa9978
Alex <a.serban@cs.ru.nl>
2020-06-03 09:42:15 +0200

    Added batch inserts.

--- graphrepo/utils.py
+++ graphrepo/utils.py
@@ -136,3 +136,120 @@ def get_method_type(method, m_before, m_current):
         return "MODIFY"
     else:
         return "ADD"
+
+
+def get_file_hash(file):
+    n = ''
+    if file.old_path and not file.new_path:
+        n = n+file.old_path
+    else:
+        n = n+file.new_path
+
+    n = n+file.filename
+    return hashlib.sha224(str(n).encode('utf-8')).hexdigest()
+
+
+def get_method_hash(method, file):
+    fhash = get_file_hash(file)
+    _fmname = fhash + "_" + method.long_name
+    return hashlib.sha224(_fmname.encode('utf-8')).hexdigest()
+
+
+def get_author_hash(email):
+    return hashlib.sha224(email.encode('utf-8')).hexdigest()
+
+
+def format_dev(a):
+    return {
+        'name': a.author.name,
+        'email': a.author.email,
+        'hash': get_author_hash(a.author.email)
+    }
+
+
+def format_commit(c, project_id):
+    return {
+        'hash': c.hash,
+        'is_merge': 1 if c.merge else 0,
+        'timestamp': c.author_date.timestamp(),
+        'project_id': project_id
+    }
+
+
+def format_branch(name, project_id):
+    return {
+        'hash':  hashlib.sha224(str(name).encode('utf-8')).hexdigest(),
+        'project_id': project_id,
+        'name': name
+    }
+
+
+def format_author_commit(a, c, timestamp):
+    return {'commit_hash': c['hash'],
+            'author_hash': a['hash'],
+            'timestamp': timestamp,
+            }
+
+
+def format_branch_commit(b, c):
+    return {'branch_hash': b, 'commit_hash': c}
+
+
+def format_file(f, project_id):
+    return {
+        'hash': get_file_hash(f),
+        'name': f.filename,
+        'project_id': project_id,
+        'type': '.' + f.filename.split('.')[-1:][0]
+    }
+
+
+def format_commit_file(c_hash, f_hash, f, timestamp):
+    return {'commit_hash': c_hash, 'file_hash': f_hash,
+            'attributes': {
+                'timestamp': timestamp,
+                'old_path': f.old_path if f.old_path else '',
+                'path': f.new_path if f.new_path else '',
+                'source_code': str(f.source_code) if f.source_code else '',
+                'source_code_before':  str(f.source_code_before) if f.source_code_before else '',
+                'nloc': f.nloc if f.nloc else -1,
+                'complexity': f.complexity if f.complexity else -1,
+                'token_count': f.token_count if f.token_count else -1,
+                'added': f.added,
+                'removed': f.removed,
+                'type': f.change_type.name}
+            }
+
+
+def format_commit_method(c_hash, m_hash, m, timestamp):
+    return {
+        'commit_hash': c_hash,
+        'method_hash': m_hash,
+        'attributes': {
+            'timestamp': timestamp,
+            'long_name': m.long_name,
+            'parameters': m.parameters,
+            'complexity': m.complexity,
+            'nloc': m.nloc,
+            'fan_in': m.fan_in,
+            'fan_out': m.fan_out,
+            'general_fan_out': m.general_fan_out,
+            'length': m.length,
+            'token_count': m.token_count,
+            'start_line': m.start_line,
+            'end_line': m.end_line
+        }
+    }
+
+
+def format_method(m, f, project_id):
+    return {
+        'hash': get_method_hash(m, f),
+        'name': m.name,
+        'file_name': m.filename,
+        'project_id': project_id
+    }
+
+
+def format_file_method(f_hash, m_hash):
+    return {'file_hash': f_hash, 'method_hash': m_hash}
