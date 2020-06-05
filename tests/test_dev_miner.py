a835d9959bafb083eb6a874bc66e5de21eebc967
NullConvergence <a.serban@cs.ru.nl>
2020-06-05 15:28:15 +0200

    Added dates to dev miner

--- tests/test_dev_miner.py
+++ tests/test_dev_miner.py
@@ -20,6 +20,7 @@ from py2neo import NodeMatcher, RelationshipMatcher
 from graphrepo.driller import Driller
 from graphrepo.utils import parse_config
 from graphrepo.miners.developer import DeveloperMiner
+from datetime import datetime
 
 
 class TestDevMiner:
@@ -27,6 +28,11 @@ class TestDevMiner:
         folder = os.path.dirname(os.path.abspath(__file__))
         test_driller = Driller(os.path.join(folder, 'cnfg_simple.yml'))
         test_driller.drill_batch()
+
+        st_date = datetime.strptime(
+            "14 May, 2020 00:00", '%d %B, %Y %H:%M').timestamp()
+        end_date = datetime.strptime(
+            "15 May, 2020 02:00", '%d %B, %Y %H:%M').timestamp()
 
         n_matcher = NodeMatcher(test_driller.graph)
         r_matcher = RelationshipMatcher(test_driller.graph)
@@ -40,22 +46,68 @@ class TestDevMiner:
             dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb")
         assert len(all_commits) == 7
 
+        all_com_id = dev_miner.get_commits(
+            dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb",
+            project_id=test_driller.config.PROJECT_ID
+        )
+        assert len(all_com_id) == 7
+
+        all_com_id_dates = dev_miner.get_commits(
+            dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb",
+            project_id=test_driller.config.PROJECT_ID,
+            start_date=st_date,
+            end_date=end_date
+        )
+        assert len(all_com_id_dates) == 7
+
         all_files = dev_miner.get_files(
             dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb"
         )
         assert len(all_files) == 6
+
+        all_files_id_dates = dev_miner.get_files(
+            dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb",
+            project_id=test_driller.config.PROJECT_ID,
+            start_date=st_date,
+            end_date=end_date
+        )
+        assert len(all_files_id_dates) == 6
 
         files_updates = dev_miner.get_files_updates(
             dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb"
         )
         assert len(files_updates) == 9
 
+        files_updates_id_dates = dev_miner.get_files_updates(
+            dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb",
+            project_id=test_driller.config.PROJECT_ID,
+            start_date=st_date,
+            end_date=end_date
+        )
+        assert len(files_updates_id_dates) == 9
+
         all_methods = dev_miner.get_methods(
             dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb"
         )
         assert len(all_methods) == 9
 
+        all_methods_id_dates = dev_miner.get_methods(
+            dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb",
+            project_id=test_driller.config.PROJECT_ID,
+            start_date=st_date,
+            end_date=end_date
+        )
+        assert len(all_methods_id_dates) == 9
+
         method_updates = dev_miner.get_method_updates(
             dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb"
         )
         assert len(method_updates) == 9
+
+        method_updates_id_dates = dev_miner.get_method_updates(
+            dev_hash="bb1a1830d2f4f4d13151827aa1072ed43bd8738a139da332e1ee3ddb",
+            project_id=test_driller.config.PROJECT_ID,
+            start_date=st_date,
+            end_date=end_date
+        )
+        assert len(method_updates_id_dates) == 9
