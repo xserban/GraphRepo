3c6b4990b9f158749b8bb6702c37ac8129e44b9b
NullConvergence <a.serban@cs.ru.nl>
2020-06-14 22:58:12 +0200

    Added intall requires to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as reqs_file:
+    requirements = reqs_file.read().splitlines()
+
 setup(name="graphrepo",
       version="0.1.5",
       description="A tool that maps a Github repo to Neo4j and Helps Mining the Repo in the DB",
       url="https://github.com/NullConvergence/GraphRepo",
       license='Apache License',
       python_requires='>=3.5',
-      packages=find_packages(),
+      install_requires=requirements,
+      packages=find_packages('.'),
       package_dir={'graphrepo': 'graphrepo'})
