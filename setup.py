cbe41c41a5d721018e74638482fe74c3876caebb
NullConvergence <a.serban@cs.ru.nl>
2020-10-09 15:48:29 +0200

    0.2.5

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-# v0.2.4 released
+# v0.2.5 released
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as reqs_file:
     requirements = reqs_file.read().splitlines()
 
 setup(name="graphrepo",
-      version="0.2.4",
+      version="0.2.5",
       description="A tool that maps a Github repo to Neo4j and Helps Mining the Repo in the DB",
       url="https://github.com/NullConvergence/GraphRepo",
       license='Apache License',
@@ -14,4 +14,5 @@ setup(name="graphrepo",
       packages=find_packages('.'),
       package_dir={'graphrepo': 'graphrepo'})
 
+# python3 setup.py sdist bdist_wheel
 # python3 -m twine upload dist/*
