a6952d035dbf2ddb3797f41056ed44f191057b95
NullConvergence <a.serban@cs.ru.nl>
2020-10-20 17:24:54 +0200

    reversed index only in query

--- graphrepo/drillers/batch_utils.py
+++ graphrepo/drillers/batch_utils.py
@@ -202,7 +202,7 @@ def merge_files(graph):
     MATCH (n1:File),(n2:File)
     WHERE n1.merge_hash = n2.merge_hash  and id(n1) < id(n2)
     WITH [n1,n2] as ns
-    ORDER BY id(ns[1]) desc
+    order by id(ns[1])
     CALL apoc.refactor.mergeNodes(ns, {properties: 'overwrite', mergeRels:true}) YIELD node
     MATCH (f:File {hash: node.hash}) -[]->(mf:Method) WITH DISTINCT f, mf
     with collect({hash: mf.hash, new_hash: f.hash}) as allRows
@@ -216,7 +216,7 @@ def merge_methods(graph):
   MATCH (n1:Method),(n2:Method)
   WHERE n1.file_name = n2.file_name and n1.name = n2.name and n1.project_id = n2.project_id and n1.merge_hash = n2.merge_hash and id(n1) < id(n2)
   WITH [n1,n2] as ns
-  ORDER BY id(ns[1]) desc
+  order by id(ns[1])
   CALL apoc.refactor.mergeNodes(ns, {properties: 'overwrite', mergeRels:true}) YIELD node
   return node
   """
@@ -288,10 +288,10 @@ def index_all(graph, developers, commits, parents, dev_commits, branches,
 
     print('Merging moved files and methods')
     start = datetime.now()
-    merge_methods(graph)
     merge_files(graph)
-
+    merge_methods(graph)
     print('Merged files and methods in \t', datetime.now()-start)
+
     print('Indexing took: \t', datetime.now()-total)
 
 
@@ -315,6 +315,6 @@ def index_cache(graph, cache, batch_size=100):
         {str(i): i for i in cache.data['file_methods']}.values()), batch_size)
     index_commit_method(graph, cache.data['commit_methods'], batch_size)
     index_commit_files(graph, cache.data['commit_files'], batch_size)
-    merge_methods(graph)
     merge_files(graph)
+    merge_methods(graph)
     print('Indexing took: \t', datetime.now()-total)
